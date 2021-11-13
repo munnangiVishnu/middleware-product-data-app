787254523b7715fe193b8331e959e53e9e5d5315
munnangiVishnu <94212133+munnangiVishnu@users.noreply.github.com>
2021-11-13 12:13:20 +0530

    Create Translate.java

---
+++ Translate.java
@@ -0,0 +1,7 @@
+public class Translate{
+  
+ public static void main(String args[]){  
+     System.out.println("This java class takes JSON data file as input and converts it to XML file as output. ");  
+    }  
+  
+}
